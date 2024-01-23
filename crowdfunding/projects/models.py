aa9434a35ed619e0e0a755ec31b0c4173b96c1b5
anyab <anyabelkina123@gmail.com>
2024-01-23 18:38:48 +0800

    Updated gitignore

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,17 +1,29 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from datetime import datetime, timedelta
+
+def one_month_from_today():
+    return datetime.now() + timedelta(days=30)
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200) #pet name
     description = models.TextField()
+    animal_choices = (
+        ("CAT", "Cat"),
+        ("DOG", "Dog"),
+        ("BIRD", "Bird"),
+        ("HORSE", "Horse")
+    )
     animal = models.CharField(max_length=30, default='unknown')
-    breed = models.CharField(max_length=30, default='unknown')
-    city = models.CharField(max_length=30, default='unknown')
+    city = models.CharField(max_length=30, choices=animal_choices, default='unknown')
+    country = models.CharField(max_length=30, default='unknown')
     goal = models.IntegerField()
     image = models.URLField()
     is_open = models.BooleanField()
-    date_created = models.DateTimeField()
+    is_successful = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=datetime.now())
+    deadline = models.DateTimeField(default=one_month_from_today)
     owner = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
