6fb5aff6ba9985a179898b5a3d1effecf2182fc6
anyab <anyabelkina123@gmail.com>
2024-01-28 17:22:09 +0800

    fixed project serializer

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -21,13 +21,14 @@ class ProjectDetailSerializer (ProjectSerializer):
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description',
-        instance.description)
+        instance.description = validated_data.get('description', instance.description)
+        instance.animal = validated_data.get('animal', instance.animal)
+        instance.city = validated_data.get('city', instance.city)
+        instance.country = validated_data.get('country', instance.country)
         instance.goal = validated_data.get('goal', instance.goal)
         instance.image = validated_data.get('image', instance.image)
         instance.is_open = validated_data.get('is_open', instance.is_open)
         instance.deadline = validated_data.get('deadline', instance.deadline)
-        instance.owner = validated_data.get('owner', instance.owner)
         instance.save()
         return instance
         
